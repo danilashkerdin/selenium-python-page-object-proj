d8868da37459ced89eee7272e51e56ea743ab6f3
danil <danilashkerdin>
2022-05-03 14:59:13 +0300

    LoginPage Page object class realized and LoginPageLocators for it added

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,10 +1,20 @@
 from selenium.webdriver.common.by import By
+import selenium.common.exceptions as exceptions
+from selenium import webdriver
 
 
 class BasePage:
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except exceptions as e:
+            return False
+        return True
